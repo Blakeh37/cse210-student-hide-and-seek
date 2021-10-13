339b851a6c89ad0963f669c91307776397778970
blakeh37 <hollibe2000@gmail.com>
2021-10-13 12:57:12 -0600

    Finished the hider file

--- hide_and_seek/game/hider.py
+++ hide_and_seek/game/hider.py
@@ -1,3 +1,21 @@
 import random
 
-# TODO: Define the Hider class here
+class Hider:
+
+    def __init__(self):
+        self.location = random.randint(1, 1000)
+        self.distance =[0, 0]
+
+    def get_hint(self):
+        if self.distance[-1] == 0:
+            message = "\nYou found me!"
+        elif self.distance[-1] < self.distance[-2]:
+            message = "\nGetting warmer!"
+        elif self.distance[-1] > self.distance[-2]:
+            message = "\nGetting colder!"
+        return message
+
+    def watch(self, location):
+        distance = abs(self.location - location)
+        self.distance.append(distance)
+        
